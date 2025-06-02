4387e13d138ce3633c0c5fae778dc8bebe95d4ec
luVAMA12 <jacqueraylucie@gmail.com>
2025-06-02 22:17:36 +0200

    continue Home.jsx, fix image overlay and font

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router"
+
 const Home = ()=> {
  return ( 
    <main id="home">
-    <div className="background-image">
-    </div>
+    <section className="hello-section">
+      <div>
+      <h1>Le 63 Restaurant</h1>
+      <p>vous souhaite la bienvenue</p>
+      </div>
+      <div className="buttons">
+      <Link to='#' className="button"> Je commande</Link>
+      </div>
+    </section>
     </main>
  )
 }
